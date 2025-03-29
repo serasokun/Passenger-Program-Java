62a6add962d21d724d9c2113c8ab5c91deb0e523
serasokun <firas.alattar.1999@gmail.com>
2025-03-29 21:50:02 +0100

    Passenger Program (Java) Main.java

--- Main.java
+++ Main.java
@@ -7,18 +7,18 @@ public class Main {
         double bagWight;
 
         int numberOfGroupes;
-        System.out.println("pleas Enter the number of Groups " );
+        System.out.println("please Enter the number of Groups " );
       
         numberOfGroupes= input.nextInt();
 
         for(int groupeCounter=1; groupeCounter<=numberOfGroupes; groupeCounter++ ) {
 
-            System.out.println("How many Guest we have here of the group number " + groupeCounter);
+            System.out.println("How many Guest of the group number " + groupeCounter+ "have we here");
             int numberOfPassengers;
             numberOfPassengers=input.nextInt();
             for (int counter = 1; counter <= numberOfPassengers; counter++) {
                 System.out.println("Passenger number " + counter);
-                System.out.println("Hello and welcome! May you chose your Class (Ff,Ee)");
+                System.out.println("Hello and welcome! May you chose your Class (Ff for First Class ,Ee for Economy)");
 
                 passClass = input.next().charAt(0);
                 System.out.println("What is your Bag Wight ? ");
