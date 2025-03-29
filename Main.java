e7cc6ede84758d25613e5c430bd78ba42f275cac
serasokun <firas.alattar.1999@gmail.com>
2025-03-29 21:38:41 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        char passClass ;
+        double bagWight;
+
+        int numberOfGroupes;
+        System.out.println("pleas Enter the number of Groups " );
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        numberOfGroupes= input.nextInt();
+
+        for(int groupeCounter=1; groupeCounter<=numberOfGroupes; groupeCounter++ ) {
+
+            System.out.println("How many Guest we have here of the group number " + groupeCounter);
+            int numberOfPassengers;
+            numberOfPassengers=input.nextInt();
+            for (int counter = 1; counter <= numberOfPassengers; counter++) {
+                System.out.println("Passenger number " + counter);
+                System.out.println("Hello and welcome! May you chose your Class (Ff,Ee)");
+
+                passClass = input.next().charAt(0);
+                System.out.println("What is your Bag Wight ? ");
+                bagWight = input.nextDouble();
+
+                double extraBagWight = 0;
+                double extraCharge = 0;
+
+                switch (Character.toUpperCase(passClass)) {
+                    case 'F':
+                        if (bagWight > 30) {
+
+                            extraBagWight = bagWight - 30;
+                            extraCharge = extraBagWight * 10;
+                        }
+                        break;
+
+                    case 'E':
+                        if (bagWight > 25) {
+                            extraBagWight = bagWight - 25;
+                            extraCharge = extraBagWight * 10;
+                        }
+                        break;
+                    default:
+                        System.out.println("Invalid choice only E or F ");
+                        continue;
+                }
+                System.out.println("extra Bag wight is  " + extraBagWight);
+                System.out.println("and the extra charge is " + extraCharge);
+
+            }
+        }
+
+    }
+}
